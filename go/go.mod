ef436c510080475925eda50d0e44ac28e738ab13
KuhnChris <kuhnchris@kuhnchris.eu>
2022-08-02 21:26:19 +0200

    Full go example

--- go/go.mod
+++ go/go.mod
@@ -3,10 +3,14 @@ module tebi-aws
 go 1.18
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.16.8 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.16.8
+	github.com/aws/aws-sdk-go-v2/config v1.15.15
+	github.com/aws/aws-sdk-go-v2/credentials v1.12.10
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.27.2
+)
+
+require (
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.3 // indirect
-	github.com/aws/aws-sdk-go-v2/config v1.15.15 // indirect
-	github.com/aws/aws-sdk-go-v2/credentials v1.12.10 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.9 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.15 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.9 // indirect
@@ -16,7 +20,6 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.10 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.9 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.13.9 // indirect
-	github.com/aws/aws-sdk-go-v2/service/s3 v1.27.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.11.13 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.16.10 // indirect
 	github.com/aws/smithy-go v1.12.0 // indirect
--- go/main.go
+++ go/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
@@ -14,41 +15,32 @@ func main() {
 	customResolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
 		return aws.Endpoint{
 			PartitionID:   "aws",
-			URL:           "http://s3.docker.tebi.io",
+			URL:           "https://s3.tebi.io",
 			SigningRegion: "de",
 		}, nil
 	})
 
-	//Credentials: credentials.StaticCredentialsProvider{Value: aws.Credentials{AccessKeyID: "", SecretAccessKey: ""}},
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithEndpointResolverWithOptions(customResolver), config.LoadOptionsFunc(func() (config.LoadOptions, err) {
+	customConfig := config.LoadOptionsFunc(func(configOptions *config.LoadOptions) error {
+		configOptions.Credentials = credentials.StaticCredentialsProvider{Value: aws.Credentials{AccessKeyID: "YOUR_ACCESS_KEY", SecretAccessKey: "YOUR_SECRET_KEY"}}
+		return nil
+	})
 
-	}))
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithEndpointResolverWithOptions(customResolver), customConfig)
 	if err != nil {
 		log.Fatalf("unable to load SDK config, %v", err)
 	}
 
 	svc := s3.NewFromConfig(cfg)
+
 	resp, err := svc.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
 	if err != nil {
 		log.Fatalf("unable to fetch buckets --> %v", err)
 	}
 
 	fmt.Println("Buckets: ")
-	for _, bucketName := range resp.Buckets {
-		fmt.Println(bucketName)
+	for _, bucketEntry := range resp.Buckets {
+		fmt.Printf("resp.Buckets entry -> %+v", bucketEntry)
+		fmt.Printf(" - real name: %v\n", aws.ToString(bucketEntry.Name))
 	}
 
-	/*
-	   // Build the request with its input parameters
-	   resp, err := svc.ListTables(context.TODO(), &dynamodb.ListTablesInput{
-	       Limit: aws.Int32(5),
-	   })
-	   if err != nil {
-	       log.Fatalf("failed to list tables, %v", err)
-	   }
-
-	   fmt.Println("Tables:")
-	   for _, tableName := range resp.TableNames {
-	       fmt.Println(tableName)
-	   }*/
 }
